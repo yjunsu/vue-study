6424a9d695fe1935e81d0d0e5d770c56771da01c
yjunsu <yjunsu@yahoo.co.jp>
2020-01-11 14:51:53 +0900

    study01 start

---
+++ study01/src/views/Document.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Topmenu/>
+<h1>Documention Page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ study01/src/views/Home.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="container">
+    <div class="notification">
+      This container is <strong>centered</strong> on desktop.
+    </div>
+    <section class="section">
+    <div class="container">
+      <h1 class="title">Section</h1>
+      <h2 class="subtitle">
+        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+      </h2>
+    </div>
+  </section>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Section</h1>
+      <h2 class="subtitle">
+        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+      </h2>
+    </div>
+  </section>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">Section</h1>
+      <h2 class="subtitle">
+        A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+      </h2>
+    </div>
+  </section>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home'
+}
+</script>
